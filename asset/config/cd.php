29b7606ad3946b928db2709d5d6d6bc6596e7023
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-05-07 14:19:39 +0900

    uqunie: asset > config > cd.php | branch > uqunie

---
+++ asset/config/cd.php
@@ -0,0 +1,35 @@
+<?php
+/**	op-unit-cd:/config.php
+ *
+ * @created    2023-01-30
+ * @license    Apache-2.0
+ * @package    op-unit-cd
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP\UNIT\CD;
+
+//	...
+return [
+	'branch' => [
+		'uqunie',
+	],
+	'ftp' => [
+		/*
+		'host'     => 'localhost',
+		'user'     => '',
+		'password' =>  null,
+		'passive'  =>  true,
+		'local'    =>  OP()->Path('git:/'),
+		'remote'   => '/var/www/htdocs/',
+		*/
+	],
+];
