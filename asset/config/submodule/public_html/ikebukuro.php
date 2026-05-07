a3912bce594d15a3bab1b09024658c089dae554d
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-05-07 14:53:52 +0900

    uqunie: asset > config > submodule > public_html > ikebukuro.php

---
+++ asset/config/submodule/public_html/ikebukuro.php
@@ -0,0 +1,25 @@
+<?php
+/**	op-skeleton-2030:/asset/config/submodule/public_html/ikebukuro.php
+ *
+ * @created    2026-02-27
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	'skip'   =>  0,
+	'path'   => 'public/ikebukuro',
+	'url'    => 'https://github.com/TomoakiNagahara/op-public-uqunie-com-ike.git',
+];
