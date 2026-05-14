64fea04909c6fc34363a600d90bb3295be255d2a
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-05-14 12:16:47 +0900

    uqunie: asset > config > submodule > layout > uqunie.php

---
+++ asset/config/submodule/layout/uqunie.php
@@ -0,0 +1,24 @@
+<?php
+/**	op-skeleton-2030:/asset/config/submodule/layout/uqunie.php
+ *
+ * @created    2026-05-14
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	'skip'   =>  0,
+	'url'    => 'https://github.com/TomoakiNagahara/op-layout-uqunie.git',
+];
