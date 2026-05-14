ba9201b0228259cd67403dabf80a6ad8a2ded6de
arch200G.uqunie.com <1668339+TomoakiNagahara@users.noreply.github.com>
2026-05-14 13:05:19 +0900

    uqunie: asset > config > _admin.php

---
+++ asset/config/_admin.php
@@ -0,0 +1,45 @@
+<?php
+/**	op-skeleton-2030:/asset/config/admin.php
+ *
+ * @created    2020-07-13
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//  ...
+$ip   = '';
+$list = [
+	'153.189.161.72', // antendo
+];
+//  Check if the IP address is in the list of allowed IPs.
+if( array_search( $_SERVER['HTTP_CF_CONNECTING_IP'], $list ) !== false ){
+	$ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+}
+
+/**	Return array of admin settings.
+ *
+ * @return array
+ */
+return [
+
+	//	Access from this IP address enables developer mode.
+	OP::_ADMIN_IP_   => $ip,
+
+	//	Error messages will be sent to this email address.
+	OP::_ADMIN_MAIL_ => 'info@onepiece-framework.com',
+
+	//	This email address will be used as the "From" address.
+	OP::_ADMIN_FROM_ => 'noreply@onepiece-framework.com',
+
+];
