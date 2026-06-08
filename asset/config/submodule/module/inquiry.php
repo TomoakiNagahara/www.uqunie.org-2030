7dd106b2ac7cff40ba42a0e045b76d462c95fd6e
TomoakiUbuntu22 WSL <1668339+TomoakiNagahara@users.noreply.github.com>
2026-06-08 11:16:29 +0900

    uqunie: asset > config > submodule > module > inquiry.php

---
+++ asset/config/submodule/module/inquiry.php
@@ -0,0 +1,24 @@
+<?php
+/**	op-skeleton-2030:/asset/config/submodule/module/inquiry.php
+ *
+ * @created    2026-06-01
+ * @license    Apache-2.0
+ * @package    op-skeleton-2030
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+return [
+	'skip'   =>  1,
+	'url'    => 'https://github.com/TomoakiNagahara/op-module-inquiry.git',
+];
