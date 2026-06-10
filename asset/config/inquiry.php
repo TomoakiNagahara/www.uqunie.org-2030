36a6271aebc6e2623b3c9a506e04cf4b828af1bf
arch200G.uqunie.com <1668339+TomoakiNagahara@users.noreply.github.com>
2026-06-10 17:35:41 +0900

    uqunie: asset > config > inquiry.php

---
+++ asset/config/inquiry.php
@@ -0,0 +1,26 @@
+<?php
+/**	op-module-inquiry:/config.php
+ *
+ * @created    2026-06-10
+ * @version    1.0
+ * @package    op-module-inquiry
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+/**	Return
+ *
+ */
+return [
+    'from' => 'no-reply@onepiece-framework.com',
+    'to'   => 'info@onepiece-framework.com',
+];
